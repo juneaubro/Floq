6cec48c1c5fcf8868bf1f29ce84a4b9b94e33c01
juneau <juneaugawat@hotmail.com>
2020-12-02 13:22:23 -0500

    Everything but follwing mouse

---
+++ Assets/Scripts/Flock Stuff/FlockScripts/FollowMouseBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/FollowMouseBehavior")]
+public class FollowMouseBehavior : FilteredFlockBehavior
+{
+    public Vector2 center;
+    public float radius = 15f;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        Vector2 centerOffset = center - (Vector2)agent.transform.position;
+
+        return centerOffset;
+    }
+}
---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+
+    public FlockAgent agentPrefab;
+    public FlockBehavior behaviour;
+
+    [Range(30, 10000)]
+    public int boids = 30;
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+    [Range(1f, 10f)]
+    public float neighborRadius = 1.5f;
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultipliter = 0.5f;
+
+    private List<FlockAgent> agents = new List<FlockAgent>();
+    private const float AgentDensity = 0.02f;
+    private float squareMaxSpeed;
+    private float squareNeighborRadius;
+    private float squareAvoidanceRadius;
+    
+    public float SquareAvoidanceRadius { get { return squareAvoidanceRadius; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighborRadius = neighborRadius * neighborRadius;
+        squareAvoidanceRadius = squareNeighborRadius * avoidanceRadiusMultipliter * avoidanceRadiusMultipliter;
+
+        for (int i = 0; i < boids; i++)
+        {
+            FlockAgent newAgent = Instantiate(agentPrefab, Random.insideUnitCircle * boids * AgentDensity, Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),transform);
+            newAgent.name = "Agent " + i;
+            newAgent.Initialize(this);
+            agents.Add(newAgent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (FlockAgent agent in agents)
+        {
+            List<Transform> context = GetNearbyObjects(agent);
+
+            Vector2 move = behaviour.CalculateMove(agent, context, this);
+
+            move *= driveFactor;
+
+            if (move.sqrMagnitude > squareMaxSpeed)
+            {
+                move = move.normalized * maxSpeed;
+            }
+
+            agent.Move(move);
+        }
+    }
+
+    List<Transform> GetNearbyObjects(FlockAgent agent)
+    {
+        List<Transform> context = new List<Transform>();
+
+        Collider2D[] contextColliders = Physics2D.OverlapCircleAll(agent.transform.position, neighborRadius);
+
+        foreach(Collider2D c in contextColliders)
+        {
+            if(c != agent.AgentCollider)
+            {
+                context.Add(c.transform);
+            }
+        }
+
+        return context;
+    }
+}
